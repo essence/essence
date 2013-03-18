6c3898d5d1c16afa36edfd93ae164452e8b73d6a
Félix Girault <felix.girault@gmail.com>
2013-03-18 21:58:39 +0100

    Added Flickr provider, fixes #9

---
+++ tests/fg/Essence/Provider/OEmbed/FlickrTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+
+namespace fg\Essence\Provider\OEmbed;
+
+if ( !defined( 'ESSENCE_BOOTSTRAPPED')) {
+	require_once dirname( dirname( dirname( dirname( dirname( __FILE__ )))))
+		. DIRECTORY_SEPARATOR . 'bootstrap.php';
+}
+
+
+
+/**
+ *	Test case for Flickr.
+ */
+
+class FlickrTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 *
+	 */
+
+	public $Flickr = null;
+
+
+
+	/**
+	 *
+	 */
+
+	public function setUp( ) {
+
+		$this->Flickr = new Flickr( );
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testCanEmbed( ) {
+
+		$this->assertTrue(
+			$this->Flickr->canEmbed( 'http://www.flickr.com/photos/phuson/4311168776/' )
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testCantEmbed( ) {
+
+		$this->assertFalse(
+			$this->Flickr->canEmbed( 'http://www.flickr.com/tour' )
+		);
+	}
+}
