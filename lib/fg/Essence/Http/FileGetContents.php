a3d378671558384df2b96962b148f681accf6788
Félix Girault <felix.girault@gmail.com>
2013-03-21 19:14:57 +0100

    Improved error reporting

--- lib/fg/Essence/Http/FileGetContents.php
+++ lib/fg/Essence/Http/FileGetContents.php
@@ -18,7 +18,19 @@ namespace fg\Essence\Http;
 class FileGetContents implements \fg\Essence\Http {
 
 	/**
+	 *	The default HTTP status code to assume if headers cannot be parsed.
+	 *
+	 *	@var int
+	 */
+
+	const defaultHttpCode = 404;
+
+
+
+	/**
 	 *	Retrieves contents from the given URL.
+	 *	Thanks to Diije for the hint on $http_response_header
+	 *	(http://www.felix-girault.fr/astuces/recuperer-une-page-web-en-php/#comment-1029).
 	 *
 	 *	@param string $url The URL fo fetch contents from.
 	 *	@return string The fetched contents.
@@ -32,8 +44,22 @@ class FileGetContents implements \fg\Essence\Http {
 		error_reporting( $reporting );
 
 		if ( $contents === false ) {
+			$code = self::defaultHttpCode;
+
+			if ( $http_response_header ) {
+				preg_match(
+					'#^HTTP/[0-9\\.]+\s(?P<code>[0-9]+)#i',
+					$http_response_header[ 0 ],
+					$matches
+				);
+
+				if ( isset( $matches['code'])) {
+					$code = $matches['code'];
+				}
+			}
+
 			// let's assume the file doesn't exists
-			throw new Exception( 404, $url );
+			throw new Exception( $code, $url );
 		}
 
 		return $contents;
