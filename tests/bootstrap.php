19134d649bcb90c3c4fc9c3a4a08d2e18a96647f
Félix Girault <felix.girault@gmail.com>
2013-07-10 22:13:46 +0200

    Removed useless resources folder

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,11 +19,6 @@ if ( !defined( 'ESSENCE_TEST' )) {
 	define( 'ESSENCE_TEST', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
 }
 
-if ( !defined( 'ESSENCE_RESOURCES' )) {
-	define( 'ESSENCE_RESOURCES', ESSENCE_TEST . 'resources' . DIRECTORY_SEPARATOR );
-}
-
 if ( !defined( 'ESSENCE_HTTP' )) {
-	define( 'ESSENCE_HTTP', ESSENCE_RESOURCES . 'http' . DIRECTORY_SEPARATOR );
+	define( 'ESSENCE_HTTP', ESSENCE_TEST . 'http' . DIRECTORY_SEPARATOR );
 }
-
