5eb621417912a24b4b3e0064d2cab67c3c78460b
Félix Girault <felix.girault@gmail.com>
2013-07-16 18:50:39 +0200

    Added a dependency injection container

---
+++ tests/Essence/Di/Container.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+
+namespace Essence\Di;
+
+
+
+/**
+ *
+ */
+
+class Containable { }
+
+
+
+/**
+ *	Test case for Container.
+ */
+
+class ContainerTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 *
+	 */
+
+	public $Container = null;
+
+
+
+	/**
+	 *
+	 */
+
+	public function setUp( ) {
+
+		$this->Container = new Container( );
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testGet( ) {
+
+		$this->Container->set( 'integer', 12 );
+		$this->assertEquals( 12, $this->Container->get( 'integer' ));
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testGetClosureResult( ) {
+
+		$this->Container->set( 'Containable', function( $Container ) {
+			return new Containable( );
+		});
+
+		$this->assertEquals( new Containable( ), $this->Container->get( 'Containable' ));
+		$this->assertNotSame( new Containable( ), $this->Container->get( 'Containable' ));
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testGetUnique( ) {
+
+		$unique = Container::unique( function( $Container ) {
+			return new Containable( );
+		});
+
+		$first = $unique( );
+		$second = $unique( );
+
+		$this->assertSame( $first, $second );
+	}
+}
