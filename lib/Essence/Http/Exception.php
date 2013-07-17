1487410ff8f3ada66fe8a50f41dae3ef0088a822
Félix Girault <felix.girault@gmail.com>
2013-07-18 00:11:50 +0200

    Shortened overly complicated error messages

--- lib/Essence/Http/Exception.php
+++ lib/Essence/Http/Exception.php
@@ -41,7 +41,8 @@ class Exception extends EssenceException {
 
 	/**
 	 *	Messages corresponding to HTTP codes.
-	 *	Thanks to Hinnerk Brügmann (http://www.multiasking.com/2011/05/http-error-codes-as-php-array/).
+	 *	Thanks to Hinnerk Brügmann
+	 *	(http://www.multiasking.com/2011/05/http-error-codes-as-php-array/).
 	 *
 	 *	@var array
 	 */
@@ -49,32 +50,32 @@ class Exception extends EssenceException {
 	protected $_messages = array(
 
 		// Client errors
-		400 => 'Bad Request - The server did not understand the request',
-		401 => 'Unauthorized - The requested page needs a username and a password',
-		402 => 'Payment Required - You can not use this code yet',
-		403 => 'Forbidden - Access is forbidden to the requested page',
-		404 => 'Not Found - The server can not find the requested page',
-		405 => 'Method Not Allowed - The method specified in the request is not allowed',
-		406 => ' Not Acceptable - The server can only generate a response that is not accepted by the client',
-		407 => 'Proxy Authentication Required - You must authenticate with a proxy server before this request can be served',
-		408 => 'Request Timeout - The request took longer than the server was prepared to wait',
-		409 => 'Conflict - The request could not be completed because of a conflict',
-		410 => 'Gone - The requested page is no longer available',
-		411 => 'Length Required - The "Content-Length" is not defined. The server will not accept the request without it',
-		412 => 'Precondition Failed - The precondition given in the request evaluated to false by the server',
-		413 => 'Request Entity Too Large - The server will not accept the request, because the request entity is too large',
-		414 => 'Request-url Too Long - The server will not accept the request, because the url is too long. Occurs when you convert a "post" request to a "get" request with a long query information',
-		415 => 'Unsupported Media Type - The server will not accept the request, because the media type is not supported',
-		416 => 'Requested Range not satisfiable',
-		417 => 'Expectation Failed',
+		400 => 'Bad request',
+		401 => 'Unauthorized',
+		402 => 'Payment required',
+		403 => 'Forbidden',
+		404 => 'Not found',
+		405 => 'Method not allowed',
+		406 => 'Not acceptable',
+		407 => 'Proxy authentication required',
+		408 => 'Request timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length required',
+		412 => 'Precondition failed',
+		413 => 'Request entity too large',
+		414 => 'Request-URL too long',
+		415 => 'Unsupported media type',
+		416 => 'Requested range not satisfiable',
+		417 => 'Expectation failed',
 
 		// Server errors
-		500 => 'Internal Server Error - The request was not completed. The server met an unexpected condition',
-		501 => 'Not Implemented - The request was not completed. The server did not support the functionality required',
-		502 => 'Bad Gateway - The request was not completed. The server received an invalid response from the upstream server',
-		503 => 'Service Unavailable - The request was not completed. The server is temporarily overloading or down',
-		504 => 'Gateway Timeout - The gateway has timed out',
-		505 => 'HTTP Version Not Supported - The server does not support the "http protocol" version'
+		500 => 'Internal server error',
+		501 => 'Not implemented',
+		502 => 'Bad gateway',
+		503 => 'Service unavailable',
+		504 => 'Gateway timeout',
+		505 => 'HTTP version not supported'
 	);
 
 
