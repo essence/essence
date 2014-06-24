8fd69c67041955fe747aa52b77898e0a4f866b78
Félix Girault <felix.girault@gmail.com>
2014-06-25 00:03:57 +0200

    Moved providers configuration out of library code

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -5,19 +5,11 @@
  *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
  */
 
-
-
-/**
- *	Definitions
- */
-
-if ( !defined( 'ESSENCE_LIB' )) {
-	define( 'ESSENCE_LIB', dirname( __FILE__ ) . DIRECTORY_SEPARATOR );
-}
-
 if ( !defined( 'ESSENCE_DEFAULT_PROVIDERS' )) {
 	define(
 		'ESSENCE_DEFAULT_PROVIDERS',
-		dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'providers.php'
+		dirname( dirname( __FILE__ ))
+		. DIRECTORY_SEPARATOR . 'config'
+		. DIRECTORY_SEPARATOR . 'providers.php'
 	);
 }
