eb3342a823304bdbdb1556378e53718a8f865d3d
Félix Girault <felix.girault@gmail.com>
2014-06-28 18:40:42 +0200

    Added Youtube presenter

---
+++ lib/Essence/Filter/Presenter/Youtube.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+
+namespace Essence\Filter\Presenter;
+
+use Essence\Media;
+
+
+
+/**
+ *
+ */
+
+class Youtube {
+
+	/**
+	 *	Available thumbnail formats.
+	 */
+
+	const small = 'small';
+	const medium = 'medium';
+	const large = 'large';
+
+
+
+	/**
+	 *	Thumbnail format.
+	 *
+	 *	@var array
+	 */
+
+	protected $_thumbnailFormat = '';
+
+
+
+	/**
+	 *	Constructor.
+	 *
+	 *	@param string $thumbnailFormat Thumbnail format.
+	 */
+
+	public function __construct( $thumbnailFormat ) {
+
+		$this->_thumbnailFormat = $thumbnailFormat;
+	}
+
+
+
+	/**
+	 *	{@inheritDoc}
+	 */
+
+	public function filter( Media $Media ) {
+
+		$url = $Media->get( 'thumbnailUrl' );
+
+		if ( $url ) {
+			switch ( $this->_thumbnailFormat ) {
+				case self::small:
+					$url = str_replace( 'hqdefault', 'default', $url );
+					break;
+
+				case self::medium:
+					$url = str_replace( 'hqdefault', 'mqdefault', $url );
+					break;
+
+				case self::large:
+				default:
+					// unchanged
+					break;
+			}
+
+			$Media->set( 'thumbnailUrl', $url );
+		}
+
+		return $Media;
+	}
+}
---
+++ tests/Essence/Filter/Presenter/YoutubeTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+
+namespace Essence\Filter\Presenter;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Essence\Media;
+
+
+
+/**
+ *	Test case for Youtube.
+ */
+
+class YoutubeTest extends TestCase {
+
+	/**
+	 *
+	 */
+
+	public $Media = null;
+
+
+
+	/**
+	 *
+	 */
+
+	public function setup( ) {
+
+		$this->Media = new Media([
+			'thumbnailUrl' => 'http://i1.ytimg.com/vi/5jmjBXoyugM/hqdefault.jpg'
+		]);
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testFilterSmall( ) {
+
+		$Youtube = new Youtube( Youtube::small );
+		$this->assertEquals(
+			'http://i1.ytimg.com/vi/5jmjBXoyugM/default.jpg',
+			$Youtube->filter( $this->Media )->get( 'thumbnailUrl' )
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testFilterMedium( ) {
+
+		$Youtube = new Youtube( Youtube::medium );
+		$this->assertEquals(
+			'http://i1.ytimg.com/vi/5jmjBXoyugM/mqdefault.jpg',
+			$Youtube->filter( $this->Media )->get( 'thumbnailUrl' )
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testFilterLarge( ) {
+
+		$Youtube = new Youtube( Youtube::large );
+		$this->assertEquals(
+			'http://i1.ytimg.com/vi/5jmjBXoyugM/hqdefault.jpg',
+			$Youtube->filter( $this->Media )->get( 'thumbnailUrl' )
+		);
+	}
+}
