2dab032773a9f05cdb73ba4d87c345ef8d90056f
Félix Girault <felix.girault@gmail.com>
2014-06-28 02:32:33 +0200

    Introduced MetaTags provider

---
+++ lib/Essence/Provider/MetaTags.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@author Laughingwithu <laughingwithu@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+
+namespace Essence\Provider;
+
+use Essence\Exception;
+use Essence\Media;
+use Essence\Provider;
+use Essence\Dom\Parser as DomParser;
+use Essence\Http\Client as HttpClient;
+
+
+
+/**
+ *	Extracts embed informations from meta tags.
+ */
+
+class MetaTags extends Provider {
+
+	/**
+	 *	Internal HTTP client.
+	 *
+	 *	@var Essence\Http\Client
+	 */
+
+	protected $_Http = null;
+
+
+
+	/**
+	 *	Internal DOM parser.
+	 *
+	 *	@var Essence\Dom\Parser
+	 */
+
+	protected $_Dom = null;
+
+
+
+	/**
+	 *	### Options
+	 *
+	 *	- 'scheme' string Scheme.
+	 */
+
+	protected $_properties = [
+		'scheme' => '#.+#'
+	];
+
+
+
+	/**
+	 *	Constructor.
+	 *
+	 *	@param Essence\Http\Client $Http HTTP client.
+	 *	@param Essence\Dom\Parser $Dom DOM parser.
+	 *	@param array $preparators Preparator.
+	 *	@param array $presenters Presenters.
+	 */
+
+	public function __construct(
+		HttpClient $Http,
+		DomParser $Dom,
+		array $preparators = [ ],
+		array $presenters = [ ]
+	) {
+		$this->_Http = $Http;
+		$this->_Dom = $Dom;
+
+		parent::__construct( $preparators, $presenters );
+	}
+
+
+
+	/**
+	 *	{@inheritDoc}
+	 */
+
+	protected function _embed( $url, array $options ) {
+
+		$html = $this->_Http->get( $url );
+		$attributes = $this->_Dom->extractAttributes( $html, [
+			'meta' => [
+				'property' => $this->scheme,
+				'content'
+			]
+		]);
+
+		if ( empty( $attributes['meta'])) {
+			throw new Exception(
+				"Unable to extract MetaTags data from '$url'."
+			);
+		}
+
+		$og = [ ];
+
+		foreach ( $attributes['meta'] as $meta ) {
+			if ( !isset( $og[ $meta['property']])) {
+				$og[ $meta['property']] = trim( $meta['content']);
+			}
+		}
+
+		return new Media( $og );
+	}
+}
---
+++ tests/Essence/Provider/MetaTags.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+
+namespace Essence\Provider;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Essence\Dom\Parser\Native as NativeDomParser;
+use Essence\Http\Client\Native as NativeHttpClient;
+
+
+
+/**
+ *	Test case for MetaTags.
+ */
+
+class MetaTagsTest extends TestCase {
+
+	/**
+	 *
+	 */
+
+	public $MetaTags = null;
+
+
+
+	/**
+	 *
+	 */
+
+	public function setUp( ) {
+
+		$this->MetaTags = new MetaTags(
+			new NativeHttpClient( ),
+			new NativeDomParser( )
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+
+	public function testEmbed( ) {
+
+		$Media = $this->MetaTags->embed(
+			'file://' . ESSENCE_HTTP . 'valid.html'
+		);
+
+		$this->assertEquals( 'og:site_name', $Media->get( 'YouTube' ));
+	}
+}
