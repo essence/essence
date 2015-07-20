0e4b3ce5d4fcee05b366e1c9673ccde10a6391d4
Félix Girault <felix.girault@gmail.com>
2015-07-20 23:47:47 +0200

    Added an utility to resolve relative URLs

---
+++ lib/Essence/Utility/Url.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+namespace Essence\Utility;
+
+
+
+/**
+ *
+ */
+class Url {
+
+	/**
+	 *	Constants identifying URL parts.
+	 */
+	const scheme = 'scheme';
+	const host = 'host';
+	const port = 'port';
+	const user = 'user';
+	const pass = 'pass';
+	const path = 'path';
+	const query = 'query';
+	const fragment = 'fragment';
+
+
+
+	/**
+	 *	Resolves relative URLs.
+	 *
+	 *	@param array $urls URLs to resolve.
+	 *	@param string $url URL of the page from which URLs
+	 *		were extracted.
+	 *	@return string Resolved URL.
+	 */
+	public static function resolve($url, $base) {
+		$urlParts = parse_url($url);
+		$baseParts = parse_url($base);
+
+		if (strpos($url, '//') === 0 && isset($baseParts[self::scheme])) {
+			return $baseParts[self::scheme] . ':' . $url;
+		}
+
+		// the URL is fully qualified
+		if (isset($urlParts[self::host])) {
+			return $url;
+		}
+
+		// the URL is absolute
+		$host = $baseParts
+			? self::host($baseParts)
+			: '';
+
+		if (strpos($url, '/') === 0) {
+			return $host . $url;
+		}
+
+		// the URL is a path
+		$basePath = isset($baseParts[self::path])
+			? $baseParts[self::path]
+			: '';
+
+		$parts = $urlParts ?: [];
+		$parts[self::path] = self::resolvePath($url, $basePath);
+
+		return $host . self::path($parts);
+	}
+
+
+
+	/**
+	 *	Resolves relative paths.
+	 *
+	 *	@param array $urlPath URLs to resolve.
+	 *	@param string $basePath URL of the page from which URLs were extracted.
+	 *	@return array Resolved path.
+	 */
+	public static function resolvePath($urlPath, $basePath) {
+		$urlParts = self::splitPath($urlPath);
+		$baseParts = self::splitPath($basePath);
+		$resolved = array_slice($baseParts, 0, -1);
+
+		foreach ($urlParts as $part) {
+			if ($part === '..') {
+				array_pop($resolved);
+			} else {
+				$resolved[] = $part;
+			}
+		}
+
+		return '/' . implode('/', $resolved);
+	}
+
+
+
+	/**
+	 *	Splits a path in parts.
+	 *
+	 *	@param string $path Path.
+	 *	@return array Parts.
+	 */
+	public static function splitPath($path) {
+		$parts = explode('/', $path);
+
+		return array_filter($parts, function($parts) {
+			return !empty($parts) && ($parts !== '.');
+		});
+	}
+
+
+
+	/**
+	 *	Builds the full host part of an URL.
+	 *
+	 *	@param array $parts URL parts.
+	 *	@return string URL.
+	 */
+	public static function host(array $parts) {
+		$url = isset($parts[self::scheme])
+			? $parts[self::scheme]
+			: 'http';
+
+		$url .= '://';
+
+		if (isset($parts[self::user]) && isset($parts[self::pass])) {
+			$url .= $parts[self::user] . ':' . $parts[self::pass] . '@';
+		}
+
+		$url .= $parts[self::host];
+
+		if (isset($parts[self::port])) {
+			$url .= ':' . $parts[self::port];
+		}
+
+		return $url;
+	}
+
+
+
+	/**
+	 *	Builds the full path part of an URL.
+	 *
+	 *	@param array $parts URL parts.
+	 *	@return string URL.
+	 */
+	public static function path($parts) {
+		$url = '/';
+
+		if (isset($parts[self::path])) {
+			$url .= ltrim($parts[self::path], '/');
+		}
+
+		if (isset($parts[self::query])) {
+			$url .= '?' . $parts[self::query];
+		}
+
+		if (isset($parts[self::fragment])) {
+			$url .= '#' . $parts[self::fragment];
+		}
+
+		return $url;
+	}
+}
---
+++ tests/Essence/Utility/UrlTest.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ *	@author Félix Girault <felix.girault@gmail.com>
+ *	@license FreeBSD License (http://opensource.org/licenses/BSD-2-Clause)
+ */
+namespace Essence\Utility;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+
+
+/**
+ *	Test case for Url.
+ */
+class UrlTest extends TestCase {
+
+	/**
+	 *
+	 */
+	public function testResolveFullyQualified() {
+		$this->assertEquals(
+			'http://test.com/a',
+			Url::resolve('http://test.com/a', 'test.com')
+		);
+
+		$this->assertEquals(
+			'https://test.com/a',
+			Url::resolve('//test.com/a', 'https://test.com')
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+	public function testResolveAbsolute() {
+		$this->assertEquals(
+			'http://test.com/a',
+			Url::resolve('/a', 'http://test.com/a/b')
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+	public function testResolveRelative() {
+		$this->assertEquals(
+			'http://test.com/b',
+			Url::resolve('b', 'http://test.com/a')
+		);
+
+		$this->assertEquals(
+			'http://test.com/a/d/f',
+			Url::resolve('../d/./e/../f', 'http://test.com/a/b/c')
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+	public function testResolvePath() {
+		$this->assertEquals(
+			'/a/c',
+			Url::resolvePath('c', '/a/b')
+		);
+
+		$this->assertEquals(
+			'/a/d/f',
+			Url::resolvePath('../d/./e/../f', '/a/b/c')
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+	public function testSplitPath() {
+		$this->assertEquals(
+			['a', '..', 'b'],
+			array_values(Url::splitPath('./a/../b'))
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+	public function testHost() {
+		$parts = [
+			Url::scheme => 'http',
+			Url::host => 'test.com',
+			Url::port => '8080',
+			Url::user => 'user',
+			Url::pass => 'pass'
+		];
+
+		$this->assertEquals(
+			'http://user:pass@test.com:8080',
+			Url::host($parts)
+		);
+	}
+
+
+
+	/**
+	 *
+	 */
+	public function testPath() {
+		$parts = [
+			Url::path => 'a',
+			Url::query => 'b=c',
+			Url::fragment => 'd'
+		];
+
+		$this->assertEquals('/a?b=c#d', Url::path($parts));
+	}
+}
